dc6ad7eb9714069a58f6d9c10ed189aa37a82305
Adil Aizhanov <adilfirstus@gmail.com>
2023-12-30 03:05:36 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const carousel = document.querySelector('.carousel');
+const slides = document.querySelectorAll('.video-slide');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let videoIndex = 0;
+
+function updateCarousel() {
+    slides.forEach((slide, index) => {
+        if (index === videoIndex) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+prevBtn.addEventListener('click', () => {
+    if (videoIndex > 0) {
+        videoIndex--;
+    } else {
+        videoIndex = slides.length - 1;
+    }
+    updateCarousel();
+});
+
+nextBtn.addEventListener('click', () => {
+    if (videoIndex < slides.length - 1) {
+        videoIndex++;
+    } else {
+        videoIndex = 0;
+    }
+    updateCarousel();
+});
+
+updateCarousel(); // Показываем первое видео при загрузке страницы
