e12d09ed31ef811adf6085af587901c8dc12d9c1
Adil Aizhanov <adilfirstus@gmail.com>
2024-01-14 18:32:50 +0600

    fix

--- script.js
+++ script.js
@@ -42,3 +42,41 @@ const menu = document.querySelector('.header-burger-menu');
 //     menuToggle.classList.toggle('active');
 //     menu.classList.toggle('active');
 // });
+
+const modal = document.getElementById('imageModal');
+const openModalBtns = document.querySelectorAll('.section-2-item-img');
+const closeModalBtn = document.getElementById('closeModalBtn');
+const modalImage = document.querySelector('#imageModal img');
+const nextImageBtn = document.getElementById('nextImageBtn');
+const prevImageBtn = document.getElementById('prevImageBtn');
+let currentIndex = 0;
+
+openModalBtns.forEach((openModalBtn, index) => {
+    openModalBtn.addEventListener('click', () => {
+        const imgSrc = openModalBtn.getAttribute('src');
+
+        modalImage.setAttribute('src', imgSrc);
+
+        currentIndex = index;
+
+        modal.style.display = 'flex';
+        document.body.classList.add('modal-open');
+    });
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+    document.body.classList.remove('modal-open');
+});
+
+nextImageBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % openModalBtns.length;
+    const imgSrc = openModalBtns[currentIndex].getAttribute('src');
+    modalImage.setAttribute('src', imgSrc);
+});
+
+prevImageBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + openModalBtns.length) % openModalBtns.length;
+    const imgSrc = openModalBtns[currentIndex].getAttribute('src');
+    modalImage.setAttribute('src', imgSrc);
+});
